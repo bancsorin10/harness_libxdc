e8e31bdccfb9ad13c07f8edb28fa73fd549ae753
sorin <bancsorin10@gmail.com>
2024-07-08 19:38:00 +0300

    decoding works

--- Makefile
+++ Makefile
@@ -1,6 +1,17 @@
 all:
 	gcc hello.c -o hello
-	gcc -fPIC -shared harness.c -o harness.so
+	gcc -Wall -Wextra -fPIC -lxdc -shared harness.c -o harness.so
+
+bitmap:
+	dd if=/dev/zero of=test_bitmap bs=1 count=$$((0x10000))
+
+clean:
+	rm hello || true
+	rm harness.so || true
+	rm aux_log || true
+	rm test_bitmap || true
+
+re: clean all bitmap run
 
 run:
 	LD_PRELOAD=./harness.so ./hello
--- harness.c
+++ harness.c
@@ -4,8 +4,22 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+#include <linux/perf_event.h>
+#include <linux/hw_breakpoint.h>
+#include <libxdc.h>
+
+#define PAGE_SIZE 0x1000
+
+#define N 3
+#define DATA_SIZE (1+(1<<N)) * PAGE_SIZE
+#define AUX_SIZE  (1<<N) * PAGE_SIZE
+
+#define BITMAP_SIZE 0x10000
 
 struct loaded_file_s {
     uint64_t base_addr;
@@ -84,8 +98,7 @@ static void *load_file(char path[], int *fd, uint64_t *size) {
 
 static void cache_map_init(
         char map[],
-        struct bin_map_s **cache_map_ptr,
-        uint64_t map_len) {
+        struct bin_map_s **cache_map_ptr) {
 
     uint64_t i;
     uint64_t j;
@@ -155,7 +168,7 @@ static struct bin_map_s *create_cache_map(char map[]) {
         ++i; 
     }
 
-    cache_map_init(map, &cache_map, map_len);
+    cache_map_init(map, &cache_map);
 
     return cache_map;
 
@@ -208,10 +221,12 @@ static void free_cache_map(struct bin_map_s *cache_map) {
 static void *fetch_cache_page(void *self_ptr, uint64_t page, bool *success) {
     struct bin_map_s *self = self_ptr;
     page &= 0xFFFFFFFFFFFFF000ULL;
-    uint64_t addr;
     uint64_t i;
 
-    if (page == self->last_addr) {
+    *success = true;
+    // printf("hello page 0x%lx\n", page);
+
+    if (page == self->last_page) {
         return (void *)self->last_addr;
     }
 
@@ -230,8 +245,9 @@ static void *fetch_cache_page(void *self_ptr, uint64_t page, bool *success) {
     }
 
     // should never be reached
-    printf("fetching failed\n");
-    return 0;
+    // printf("fetching failed\n");
+    *success = false;
+    return NULL;
 }
 
 static void debug_print_cache_map(struct bin_map_s *cache_map) {
@@ -251,16 +267,154 @@ static void debug_print_cache_map(struct bin_map_s *cache_map) {
     }
 }
 
+// get code of the dynamic PMU provided by the intel_pt driver
+static uint32_t get_intelpt_type() {
+    int fd;
+    char type[2];
+
+    fd = open("/sys/bus/event_source/devices/intel_pt/type", O_RDONLY);
+    read(fd, type, 2);
+    close(fd);
+    return (uint32_t)((type[0]-'0')*10 + type[1]-'0');
+}
+
+static int open_perf_event(int pid) {
+    int fd;
+    struct perf_event_attr attr;
+
+    memset(&attr, 0, sizeof(attr));
+
+    // read type from `/sys/bus/event_source/devices/intel_pt/type`
+    // dynamic PMU for intel pt
+    attr.type           = get_intelpt_type();
+    attr.size           = sizeof(attr);
+    attr.disabled       = 1;
+    attr.exclude_kernel = 1;
+    attr.exclude_hv     = 1;
+    attr.exclude_idle   = 1;
+
+    // attr.inherit        = 1;
+    // attr.exclude_guest  = 1;
+    // attr.sample_id_all  = 1;
+    // attr.sample_period  = 1;
+
+    // stolen with
+    // `perf --debug verbose=2 record -e intel_pt/cyc=0,tsc=0,mtc=0,noretcomp=1/u ./hello`
+    attr.config = 0x300e801;
+
+    fd = syscall(SYS_perf_event_open, &attr, pid, -1, -1, 0);
+    if (fd == -1) {
+        printf("event open failed\n");
+        exit(0);
+    }
+
+    return fd;
+}
+
+static void perf_allocate_buffers(
+        int perf_fd,
+        void **data_ptr,
+        void **aux_ptr) {
+
+    struct perf_event_mmap_page *header;
+    void *base;
+    void *aux;
+
+    base = mmap(NULL, DATA_SIZE, PROT_WRITE, MAP_SHARED, perf_fd, 0);
+    if (base == MAP_FAILED) {
+        printf("base mmap failed\n");
+        exit(0);
+    }
+
+    header = base;
+
+    header->aux_offset = header->data_offset + header->data_size;
+    header->aux_size    = AUX_SIZE;
+
+    aux = mmap(NULL, header->aux_size, PROT_WRITE, MAP_SHARED, perf_fd,
+               header->aux_offset);
+    if (aux == MAP_FAILED) {
+        printf("aux mmap failed\n");
+        exit(0);
+    }
+
+    *data_ptr = base;
+    *aux_ptr  = aux;
+}
+
+static void *load_bitmap(int *bitmap_fd) {
+    // bitmap is loaded from a shared memory region / a file
+    // the file name should be in an environment variable at least for
+    // afl++
+    // as a demo create a file with
+    // `dd if=/dev/zero of=test_bitmap bs=1 count=$((0x10000))`
+
+    int fd;
+    void *bitmap;
+
+    fd = open("test_bitmap", O_RDWR);
+    if (fd <= 0) {
+        printf("failed reading the bitmap\n");
+        exit(0);
+    }
+
+    // int stat_res;
+    // struct stat sb;
+    // stat_res = fstat(fd, &sb);
+    // if (stat_res < 0) {
+    //     printf("stat failed in bitmap\n");
+    //     exit(0);
+    // }
+    //
+    // bitmap = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    // size of file is known
+    bitmap = mmap(
+            NULL,
+            BITMAP_SIZE,
+            PROT_READ | PROT_WRITE,
+            MAP_SHARED,
+            fd,
+            0);
+
+    *bitmap_fd = fd;
+
+    return bitmap;
+}
+
+static void debug_log_aux(void *aux, uint64_t size) {
+    int fd;
+
+    fd = open("aux_log", O_WRONLY | O_CREAT, 0666);
+    write(fd, aux, size);
+    close(fd);
+}
+
+static uint64_t get_aux_size(void *data_ptr) {
+    uint8_t *data;
+
+    data = (uint8_t *)data_ptr;
+    data += ((struct perf_event_mmap_page *)data)->data_offset;
+    struct perf_event_header *h;
+
+    while ((h = (struct perf_event_header *)data)->type != 0) {
+        if (h->type == PERF_RECORD_AUX) {
+            return *(uint64_t *)
+                (data + sizeof(struct perf_event_header) + 8);
+        }
+        data += h->size;
+    }
+    return 0;
+}
+
 static __attribute__((constructor)) void main(int ac, char **av) {
 
     char map[4096];
     uint64_t main_off = 0x1135;
-    uint64_t base;
-    uint64_t off;
     struct bin_map_s *cache_map;
-    bool succ;
 
     read_map(getpid(), map);
+    // printf("%s\n", map);
 
     cache_map = create_cache_map(map);
 
@@ -269,23 +423,99 @@ static __attribute__((constructor)) void main(int ac, char **av) {
         exit(0);
     }
 
-    debug_print_cache_map(cache_map);
+    // debug_print_cache_map(cache_map);
     
+    // declare the main function
     int (*main_func)(int, char **) = ((int (*)(int, char **))(cache_map->loaded[0]->base_addr - cache_map->loaded[0]->offset + main_off));
 
-    printf("main addr %p\n", main_func);
-    printf("main page fetched %p\n", fetch_cache_page((void *)cache_map, (uint64_t)(void *)main_func, &succ));
+    // setup intel_pt perf
+    int perf_fd;
+    void *data;
+    void *aux;
+
+    perf_fd = open_perf_event(getpid());
+    perf_allocate_buffers(perf_fd, &data, &aux);
+
+    // enable intel_pt
+    ioctl(perf_fd, PERF_EVENT_IOC_RESET, 0);
+    ioctl(perf_fd, PERF_EVENT_IOC_ENABLE, 0);
+
+    // run main
     main_func(ac, av);
-    exit(0);
 
-    int i;
+    // disable intel_pt
+    ioctl(perf_fd, PERF_EVENT_IOC_DISABLE, 0);
 
-    // afl init
-    // afl loop
-    for (i = 0; i < 10; ++i) {
-        printf("calling main from orig: ");
-        main_func(ac, av);
+    uint64_t curr_aux_size;
+    curr_aux_size = get_aux_size(data);
+
+    // debug and check collected trace
+    // debug_log_aux(aux, AUX_SIZE);
+    // debug_log_aux(data);
+
+    
+    // setup decoding
+    libxdc_t *decoder;
+    uint64_t filter[4][2] = {0}; // cr3 filters not supported by perf?
+    void *bitmap;
+    int bitmap_fd;
+    decoder_result_t ret;
+
+    // still need to add some filter
+    // filter[0][0] = 0x500000000000;
+    // filter[0][1] = 0x7fffffffffff;
+
+    // we don't really care we kinda mapped everything
+    // there should be no problems
+    filter[0][0] = 0x1;
+    filter[0][1] = 0xffffffffffffffff;
+
+    bitmap = load_bitmap(&bitmap_fd);
+
+    decoder = libxdc_init(
+            filter,
+            &fetch_cache_page,
+            cache_map,
+            bitmap,
+            BITMAP_SIZE);
+
+    // can't write to trace
+    // ((uint8_t *)aux)[AUX_SIZE-1] = 0x55;
+    uint8_t *trace;
+    trace = (uint8_t *)malloc(curr_aux_size + 1);
+    memcpy(trace, aux, curr_aux_size);
+    trace[curr_aux_size] = 0x55;
+    debug_log_aux(trace, curr_aux_size+1);
+
+    printf("aux %p-%p size 0x%lx\n", trace, trace+curr_aux_size, curr_aux_size);
+
+    ret = libxdc_decode(decoder, trace, curr_aux_size);
+    if (ret) {
+        printf("decoding failed %d\n", ret);
     }
+
+
+cleanup:
+    if (trace) {
+        free(trace);
+    }
+
+    if (bitmap) {
+        munmap(bitmap, BITMAP_SIZE);
+        close(bitmap_fd);
+    }
+
+    if (data) {
+        munmap(data, DATA_SIZE);
+    }
+
+    if (aux) {
+        munmap(aux, AUX_SIZE);
+    }
+
+    close(perf_fd);
+
     free_cache_map(cache_map);
+
     exit(0);
 }
